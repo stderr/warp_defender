55b0ce958e3c1c0c895db3777aeddf961edb34c0
Brad Anderson <eco@gnuk.net>
2011-12-03 15:33:20 -0700

    Fix tabs

--- lib/game_states/paused.rb
+++ lib/game_states/paused.rb
@@ -1,33 +1,33 @@
 module GameStates
 
-	class Paused < GameState
+  class Paused < GameState
 
     def initialize(window, game_engine)
-			super(window, game_engine)
-			@play_state = @game_engine.states.reverse.detect { |state| state.class == Playing }
-		end
+      super(window, game_engine)
+      @play_state = @game_engine.states.reverse.detect { |state| state.class == Playing }
+    end
 
-		def draw
-			@play_state.draw
-			color = Gosu::Color.from_ahsv(200, 0, 0, 0)
-			@window.draw_quad(0, 0, color,
-												@window.width, 0, color,
-												0, @window.height, color,
-												@window.width, @window.height, color,
-												Utils::ZOrder::PauseOverlay)
+    def draw
+      @play_state.draw
+      color = Gosu::Color.from_ahsv(200, 0, 0, 0)
+      @window.draw_quad(0, 0, color,
+                        @window.width, 0, color,
+                        0, @window.height, color,
+                        @window.width, @window.height, color,
+                        Utils::ZOrder::PauseOverlay)
 
       position_x = @window.width / 2
       position_y = @window.height / 2
       @window.fonts[:menu].draw_rel("Paused", position_x, position_y - 40, 0, 0.5, 1, 2, 2)
 
-		end
+    end
 
     def button_down(id)
-    	if id == Gosu::KbEscape
-    		leave
-			end
+      if id == Gosu::KbEscape
+        leave
+      end
     end
-	end
+  end
 
 end
 
