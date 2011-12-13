96bb07b9d4ce6e14684e9869b388d2b8c9da6dac
Joshua Fenio <std3rr@gmail.com>
2011-12-13 16:29:09 -0700

    * Refactored retarded Minimap (was I drinking?) * Implemented first iteration of HUD

--- lib/gui/mini_map.rb
+++ lib/gui/mini_map.rb
@@ -8,24 +8,19 @@ module GUI
 
     def draw(x, y, options = {})
       super(x, y, options)
-
-      $window.draw_quad(x, y, @options[:color],
-                       x+@options[:width], y, @options[:color],
-                       x+@options[:width], y-@options[:height], @options[:color],
-                       x, y-@options[:height], @options[:color],
-                       Utils::ZOrder::HUD)
+      
+      $window.draw_rect(x, y, 
+                        @options[:width], @options[:height],
+                        @options[:color], Utils::ZOrder::HUD)
 
       @options[:entities].each do |entity|
         next if entity.is_a?(Entities::Bullet) || entity.off_screen?
+        
+        # Compensate for rectangle drawing from center
+        pos_x = (x-@options[:width]/2) + ((entity.x * @options[:width]) / $window.width) 
+        pos_y = (y-@options[:height]/2) + ((entity.y * @options[:height]) / $window.height)
 
-        pos_x = x+(entity.x/10)
-        pos_y = (y - @options[:height]) + (entity.y/10)
-
-        $window.draw_quad(pos_x, pos_y, entity.map_color,
-                         pos_x+2, pos_y, entity.map_color,
-                         pos_x+2, pos_y-2, entity.map_color,
-                         pos_x, pos_y-2, entity.map_color,
-                         Utils::ZOrder::HUDOverlay)
+        $window.draw_rect(pos_x, pos_y, 5, 5, entity.map_color, Utils::ZOrder::HUDOverlay)
       end
 
     end
