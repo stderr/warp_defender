3cb6a0440b2158669ba47a0b3b7e796b4604d910
Joshua Fenio <std3rr@gmail.com>
2011-12-17 22:33:29 -0700

    Oops, forgot to actually add in the states.  As well, exchanged old font for a sleeker one.

---
+++ lib/game_states/game_completed.rb
@@ -0,0 +1,31 @@
+module GameStates
+
+  class GameCompleted < GameState
+
+    def initialize(game_engine)
+      super(game_engine)
+      @play_state = @game_engine.states.reverse.detect { |state| state.class == Playing }
+    end
+
+    def draw
+      @play_state.draw
+      color = Gosu::Color.from_ahsv(200, 0, 0, 0)
+
+      $window.draw_rect($window.width/2, $window.height/2, 
+                        $window.width, $window.height, 
+                        color, Utils::ZOrder::PauseOverlay)
+                        
+      position_x = $window.width / 2
+      position_y = $window.height / 2
+      $window.fonts[:menu].draw_rel("You Win!", position_x, position_y - 40, Utils::ZOrder::PauseOverlay, 0.5, 1, 2, 2)
+    end
+
+    def button_down(id)
+      case id
+      when Gosu::KbQ
+        Process.exit
+      end
+    end
+  end
+
+end
